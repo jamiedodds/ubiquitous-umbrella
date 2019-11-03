a3949ad55b707639ca054bb58e45826e0d8e8c36
jamiedodds <jamiedodds13@gmail.com>
2019-11-03 14:29:53 +0000

    some styling and auth done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fgci1-1.fna.fbcdn.net/vp/a5dd6ae64bae3281cab51f3f8abb18ed/5E5AF927/t51.2885-19/s320x320/70985486_577637296311063_2240788552625422336_n.jpg?_nc_ht=instagram.fgci1-1.fna.fbcdn.net" alt="logo" class="rounded-circle" style="width: inherit;">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">freecodecamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fgci1-1.fna.fbcdn.net/vp/beb4fbc85a11bf9cfae41e853f39f282/5E60A8E5/t51.2885-15/sh0.08/e35/c0.108.1440.1440a/s640x640/75266943_172640417127046_8922705727225766606_n.jpg?_nc_ht=instagram.fgci1-1.fna.fbcdn.net&_nc_cat=110" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fgci1-1.fna.fbcdn.net/vp/d8d5a9e0142201fc2ad5f682517fe0ad/5E444480/t51.2885-15/sh0.08/e35/c0.115.933.933a/s640x640/73319948_3118988548118480_2584593794372883946_n.jpg?_nc_ht=instagram.fgci1-1.fna.fbcdn.net&_nc_cat=108" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fgci1-1.fna.fbcdn.net/vp/7ad2f3096c7b0628cf86e5597de839f5/5E5B7769/t51.2885-15/e35/c93.0.563.563a/73427310_857771071310415_1803775118571694506_n.jpg?_nc_ht=instagram.fgci1-1.fna.fbcdn.net&_nc_cat=103" alt="" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection

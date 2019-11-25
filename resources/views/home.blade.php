5d25b62e442cfbcecf7928d03b2691c72b70f290
jamiedodds <jamiedodds13@gmail.com>
2019-11-25 20:58:47 +0000

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,18 @@
             <img src="https://instagram.fgci1-1.fna.fbcdn.net/vp/a5dd6ae64bae3281cab51f3f8abb18ed/5E5AF927/t51.2885-19/s320x320/70985486_577637296311063_2240788552625422336_n.jpg?_nc_ht=instagram.fgci1-1.fna.fbcdn.net" alt="logo" class="rounded-circle" style="width: inherit;">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>{{$user->username}}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{$user->username}}</h1>
+                <a href="#">Add New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">freecodecamp.org</div>
-            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
-            <div><a href="#">www.freecodecamp.org</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
     <div class="row pt-5">
